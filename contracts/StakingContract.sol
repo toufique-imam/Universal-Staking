2eafd6eb71f8e7ec0642dd717daeb29b4698b39c
Nuhash <nuhash1083s@gmail.com>
2023-09-13 15:28:03 +0600

    Revert "up"
    This reverts commit d685f801a3d67c8419530f4ea61403e16add6b76.

---
+++ contracts/StakingContract.sol
@@ -0,0 +1,220 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/interfaces/IERC20.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+
+contract StakingContract is Ownable , Pausable {
+    // Structure to represent staking pools
+    struct StakingPool {
+        address stakingToken;
+        uint8 stakingTokenDecimals;
+        address rewardToken;
+        uint8 rewardTokenDecimals;
+        uint256 totalRewards;
+        uint256 startDate;
+        uint256 endDate;
+        address creator;
+        uint8 stakingFeePercentage;
+        uint8 unstakingFeePercentage;
+        uint8 maxStakingFeePercentage;
+        uint256 bonusPercentage;
+        uint256 maxStakePerWallet;
+        bool isActive;
+        uint256 penaltyPercentage;
+    }
+
+    // Mapping to track staking pools
+    mapping(uint256 => StakingPool) public stakingPools;
+    uint256 public poolCount;
+
+    // Mapping to track user staked balances
+    mapping(address => mapping(uint256 => uint256)) public stakedBalances;
+
+    // Mapping to track user rewards
+    mapping(address => mapping(uint256 => uint256)) public rewards;
+
+    // Mapping to track user unstaked balances
+    mapping(address => mapping(uint256 => uint256)) public unstakedBalances;
+
+    // Mapping to track active pools
+    mapping(uint256 => bool) public isActivePool;
+    event PoolCreated(uint256 poolId);
+    event Staked(address indexed user, uint256 indexed poolId, uint256 amount);
+    event Unstaked(address indexed user, uint256 indexed poolId, uint256 amount, uint256 penalty);
+    event RewardClaimed(address indexed user, uint256 indexed poolId, uint256 amount);
+    // Constructor to set the contract owner
+    constructor() {
+    }
+
+    // Function to create a staking pool
+    function createStakingPool(
+        address _stakingToken,
+        uint8 _stakingTokenDecimals,
+        address _rewardToken,
+        uint8 _rewardTokenDecimals,
+        uint256 _bonusPercentage,
+        uint256 _startDate,
+        uint256 _endDate,
+        uint8 _stakingFeePercentage,
+        uint8 _unstakingFeePercentage,
+        uint8 _maxStakingFeePercentage,
+        uint256 _maxStakePerWallet,
+        uint256 _penaltyPercentage
+    ) external whenNotPaused {
+        require(_stakingFeePercentage <= 1, "Staking fee cannot exceed 1%");
+        require(_unstakingFeePercentage <= 100, "Unstaking fee cannot exceed 100%");
+        require(_maxStakingFeePercentage <= 1, "Max staking fee cannot exceed 1%");
+
+        uint256 poolId = poolCount;
+        stakingPools[poolId] = StakingPool(
+            _stakingToken,
+            _stakingTokenDecimals,
+            _rewardToken,
+            _rewardTokenDecimals,
+            0,
+            _startDate,
+            _endDate,
+            msg.sender,
+            _stakingFeePercentage,
+            _unstakingFeePercentage,
+            _maxStakingFeePercentage,
+            _bonusPercentage,
+            _maxStakePerWallet,
+            true,
+            _penaltyPercentage
+        );
+        isActivePool[poolId] = true;
+        poolCount++;
+
+        emit PoolCreated(poolId);
+    }
+
+    // Function for users to stake tokens
+    function stake(uint256 _poolId, uint256 _amount) external whenNotPaused {
+        // Check if the pool is active
+        require(isActivePool[_poolId], "This pool is not active");
+
+        // Check if the staking period is valid
+        require(block.timestamp >= stakingPools[_poolId].startDate && block.timestamp <= stakingPools[_poolId].endDate, "Staking period is not valid");
+
+        // Check if the user's staked balance doesn't exceed the maximum allowed
+        require(stakedBalances[msg.sender][_poolId] + _amount <= stakingPools[_poolId].maxStakePerWallet, "Exceeded maximum stake limit");
+        // Make sure to approve the contract to spend the tokens beforehand
+        require(IERC20(stakingPools[_poolId].stakingToken).allowance(msg.sender, address(this)) >= _amount, "Please approve the contract to spend the tokens first");
+        // Transfer staking tokens from the user to the contract
+        IERC20(stakingPools[_poolId].stakingToken).transferFrom(msg.sender, address(this), _amount);
+        
+        // Calculate staking fee
+        uint256 stakingFee = (_amount * stakingPools[_poolId].stakingFeePercentage) / 100;
+
+        // Calculate net staked amount
+        uint256 netStakedAmount = _amount - stakingFee;
+
+        // Update user's staked balance
+        stakedBalances[msg.sender][_poolId] += netStakedAmount;
+
+        // Update total staked tokens in the pool
+        stakingPools[_poolId].totalRewards += netStakedAmount;
+
+        // Emit stake event
+        emit Staked(msg.sender, _poolId, _amount);
+    }
+
+    // Function for users to unstake tokens
+    function unstake(uint256 _poolId, uint256 _amount) external whenNotPaused {
+        // Check if the pool is active
+        require(isActivePool[_poolId], "This pool is not active");
+
+        // Check if the user has enough staked tokens
+        require(stakedBalances[msg.sender][_poolId] >= _amount, "Insufficient staked balance");
+
+        // Check if the staking period is valid
+        require(block.timestamp >= stakingPools[_poolId].startDate, "Unstaking is not allowed before the staking period starts");
+
+        // Calculate unstaking fee
+        uint256 unstakingFee = (_amount * stakingPools[_poolId].unstakingFeePercentage) / 100;
+
+        // Calculate penalty for early unstaking
+        uint256 penalty = 0;
+        if (block.timestamp < stakingPools[_poolId].endDate) {
+            penalty = (_amount * stakingPools[_poolId].penaltyPercentage) / 100;
+        }
+
+        // Calculate net unstaked amount
+        uint256 netUnstakedAmount = _amount - unstakingFee - penalty;
+
+        // Transfer staking tokens back to the user
+        IERC20(stakingPools[_poolId].stakingToken).transfer(msg.sender, netUnstakedAmount);
+        
+        //calculate rewards based on amount of time the user staked and amount staked
+        uint256 timeStaked = block.timestamp - stakingPools[_poolId].startDate;
+        uint256 timeStakedInDays = timeStaked / 86400;
+
+        // Transfer rewards to the user
+        IERC20(stakingPools[_poolId].rewardToken).transfer(msg.sender, rewards[msg.sender][_poolId]); 
+
+        // Update user's staked balance
+        stakedBalances[msg.sender][_poolId] -= _amount;
+
+        // Update total staked tokens in the pool
+        stakingPools[_poolId].totalRewards -= _amount;
+
+        // Emit unstake event
+        emit Unstaked(msg.sender, _poolId, _amount, penalty);
+    }
+
+    // Function to check if a pool exists
+    function poolExists(uint256 _poolId) external view returns (bool) {
+        return _poolId < poolCount;
+    }
+
+    // Function to check if a pool is active
+    function poolIsActive(uint256 _poolId) external view returns (bool) {
+        return isActivePool[_poolId];
+    }
+
+    // Function to get pool information
+    function getPoolInfo(uint256 _poolId)
+        external
+        view
+        returns (
+            address,
+            uint8,
+            address,
+            uint8,
+            uint256,
+            uint256,
+            uint256,
+            address,
+            uint8,
+            uint8,
+            uint8,
+            uint256,
+            bool
+        )
+    {
+        StakingPool storage pool = stakingPools[_poolId];
+        return (
+            pool.stakingToken,
+            pool.stakingTokenDecimals,
+            pool.rewardToken,
+            pool.rewardTokenDecimals,
+            pool.totalRewards,
+            pool.startDate,
+            pool.endDate,
+            pool.creator,
+            pool.stakingFeePercentage,
+            pool.unstakingFeePercentage,
+            pool.maxStakingFeePercentage,
+            pool.maxStakePerWallet,
+            pool.isActive
+        );
+    }
+
+    // Function to set a pool as inactive (emergency shutdown)
+    function setPoolInactive(uint256 _poolId) external onlyOwner {
+        isActivePool[_poolId] = false;
+    }
+}
